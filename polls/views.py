47863045826f04bf7644ac3e11aed4bd3545b3bc
JAY <johnjohnsonokah@gmail.com>
2018-11-24 10:38:01 +0100

    Namespacing URL names (with url error - NoReverseMatch)

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from .models import Question
 
@@ -13,11 +13,8 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question_id})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You 're looking at the results of question %s."
