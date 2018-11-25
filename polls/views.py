7cea3602b4ed1b339f6324acf9190e8a5d80e49b
JAY <johnjohnsonokah@gmail.com>
2018-11-25 06:08:30 +0100

    fixed url error - 'polls/<int...>/' detail

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,14 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render, get_object_or_404
-from django.template import loader
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
